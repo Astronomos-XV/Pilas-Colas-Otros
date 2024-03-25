65fc334db3a1f9d15154ee9d96b4ff0ae1abb625
Astronomos-XV <74269535+Astronomos-XV@users.noreply.github.com>
2024-03-25 19:34:16 -0400

    Add files via upload

---
+++ SearchValue/index.js
@@ -0,0 +1,161 @@
+//Creamos un nodo
+
+class Node {
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.size = 0;
+        this.head = null;
+    }
+
+    getSize(){
+        return this.size;
+    }    
+
+    isEmpty(){
+        return this.size === 0;
+    }
+
+    insertFirst(data){
+        const node = new Node(data);
+        if (this.isEmpty()){
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    insertLast(data){
+        const node = new Node(data);
+        if (this.isEmpty()){
+            this.head = node;
+        } else {
+            let current = this.head;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+
+    insert(data, index){
+        if (index < 0 || index > this.size){
+            console.log("Índice inválido");
+            return;
+        }
+        if (index === 0){
+            this.insertFirst(data);
+        } else {
+            const node = new Node(data);
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++){
+                prev = prev.next;
+            }
+            node.next = prev.next;
+            prev.next = node;
+            this.size++;
+        }
+    }
+
+    removeIndex(index){
+        if (index < 0 || index >= this.size) {
+            console.log("Índice inválido.");
+            return null;
+        }
+        if (index === 0){
+            this.head = this.head.next;
+        } else {
+            let prev = this.head;
+            for(let i = 0; i < index - 1; i++){
+                 prev = prev.next;
+            }
+            prev.next = prev.next.next;
+        }
+        this.size--;
+    }
+
+    searchIndex(index){
+        if (index < 0 || index > this.size) {
+            console.log("Índice inválido.");
+            return null;
+        }
+        let current = this.head;
+        for (let i = 0; i < index; i++){
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    printAll(){
+        if (this.isEmpty()){
+            console.log("La lista está vacía");
+           
+        } else {
+            let current = this.head;
+            let result = "";
+            while(current){
+                result += current.data + ' -> ';
+                current = current.next;
+            }
+            console.log(result + ' null')
+        }
+        
+    }
+
+
+
+//Extra añadido 
+buscarvalor(value) {
+    let similar = 0;
+    let pasada = 0;
+    for (let i = 1; i < this.size + 1; i++) {
+        similar = this.searchIndex(i);
+        if (value === similar) {
+            return i;
+        }
+        pasada++;
+    }
+    if (pasada === this.size) {
+        console.log("valor no encontrado.");
+        return null;
+    }
+}
+
+
+}
+
+
+
+let lista = new LinkedList();
+console.log(lista.isEmpty());
+lista.printAll();
+lista.insertFirst("José");
+lista.printAll();
+lista.insertLast("Pepito"); 
+lista.printAll();
+lista.insertFirst("Manolo");
+lista.printAll()
+lista.insertLast("Barack Obama");
+lista.printAll();
+lista.insert("IWEJHGDWSIKJUGH", 3);
+lista.printAll();
+//lista.removeIndex(3);
+lista.printAll();
+lista.insert({
+    nombre: "George Bush",
+    categoria: "Terrorista"
+}, 2)
+//console.log(lista.searchIndex(3));
+
+console.log(lista.buscarvalor("José"));
+
+
+
